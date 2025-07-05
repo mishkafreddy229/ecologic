c201ec2a11cba58f5f7ca8047c53ac7a3322caf5
lol4ik <mishkafredya229@gmail.com>
2025-07-05 19:26:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ def say_hello(message):
 
 @bot.message_handler(commands=["eco"])
 def eco_items(message):
-    bot.send_message(message.chat.id,'выбери предмет, для которого хочешь узнать, сколько он разлагается', reply_markup=get_inline_keyboard())
+    bot.send_message(message.chat.id,'выбери предмет, для которого хочешь узнать, сколько он разлагается или что он плохово делает', reply_markup=get_inline_keyboard())
 
 @bot.callback_query_handler(func=lambda call: call.data in ["btn1", "btn2", "btn3", "btn4", "btn5"])
 def handle_infinity_keyboard(call):
     items = {
-        "btn1" : "450 лет",
-        "btn2" : "до 2 лет",
-        "btn3" : "более 1000 лет",
-        "btn4" : "500 лет",
-        "btn5" : "2 года"
+        "btn1" : "450 лет, оказывают негативное воздействие на окружающую среду",
+        "btn2" : "до 2 лет, Запах , Привлечение насекомых",
+        "btn3" : "более 1000 лет, Загрязнение окружающей среды, иза осколков если об них случайно зацепится то поранишься, Пожароопасность",
+        "btn4" : "500 лет, Загрязнение окружающей среды, иза осколков если об них случайно зацепится то поранишься, Пожароопасность",
+        "btn5" : "2 года, вырубку лесов, загрязнение воды и воздуха, а также образование отходов"
     }
     bot.answer_callback_query(call.id)
     bot.send_message(call.message.chat.id, items[call.data])
